9733387bf63d41623220275b8de445c09183d7bf
tata <TaWang@ashleyfurniture.com>
2023-07-21 16:08:02 +0800

    feat: init

---
+++ core/internal/logic/frontend/customerchatlogic.go
@@ -0,0 +1,44 @@
+package frontend
+
+import (
+	"context"
+	"core/internal/service/ai"
+	"core/internal/service/structure"
+	"fmt"
+
+	"core/internal/svc"
+	"core/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CustomerChatLogic struct {
+	logx.Logger
+	ctx         context.Context
+	svcCtx      *svc.ServiceContext
+	chatService *ai.ChatService
+}
+
+func NewCustomerChatLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CustomerChatLogic {
+	return &CustomerChatLogic{
+		Logger:      logx.WithContext(ctx),
+		ctx:         ctx,
+		svcCtx:      svcCtx,
+		chatService: ai.NewChatService(),
+	}
+}
+
+func (l *CustomerChatLogic) CustomerChat(req *types.CustomerChatRequest) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+	open := ai.NewOpenAi(l.ctx, l.svcCtx.Config)
+	l.chatService.Set(open)
+	info, err := l.chatService.Chat([]structure.ChatModelMessage{
+		{
+			Role:    "user",
+			Content: "aaa",
+		},
+	})
+	fmt.Println(info)
+	fmt.Println(err)
+	return
+}
---
+++ core/internal/service/ai/chat.go
@@ -0,0 +1,27 @@
+package ai
+
+import "core/internal/service/structure"
+
+type ChatService struct {
+	strategy ChatAi
+}
+
+func NewChatService() *ChatService {
+	return &ChatService{}
+}
+
+func (c *ChatService) Set(strategy ChatAi) {
+	c.strategy = strategy
+}
+
+func (c *ChatService) Chat(request []structure.ChatModelMessage) (string, error) {
+	return c.strategy.Chat(request)
+}
+
+func (c *ChatService) ChatStream(request []structure.ChatModelMessage, channel chan string) (string, error) {
+	return c.strategy.ChatStream(request, channel)
+}
+
+func (c *ChatService) Completion(request string) (string, error) {
+	return c.strategy.Completion(request)
+}
